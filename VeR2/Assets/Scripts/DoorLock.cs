a62bce3f7c17199975e39e9078ac35d4491ce44e
Stephen Ogden <42942300+sjogden@users.noreply.github.com>
2018-11-30 22:04:02 -0500

    Interactable door physics
    Door now functional and interactable.
    Door snaps shut at <30 degrees.
    Started lock mechanics.

---
+++ VeR2/Assets/Scripts/DoorLock.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorLock : MonoBehaviour {
+    private const double min = 0.195;
+    private const double max = 0.196;
+    private bool locked;
+    private Rigidbody door;
+	// Use this for initialization
+	void Start () {
+        door = GetComponent<Rigidbody>();
+        locked = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (locked)
+        {
+            door.constraints.
+        }
+	}
+
+    public bool isLocked ()
+    {
+        return locked;
+    }
+
+    public void setLocked()
+    {
+        if (transform.rotation.y < max && transform.rotation.y > min)
+        {
+            locked = !locked;
+        }
+    }
+}
---
+++ VeR2/Assets/Scripts/DoorSnapShut.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorSnapShut : MonoBehaviour {
+    private const double minAngle = 30.0 / 360.0 + 0.1950903;
+    private HingeJoint hinge;
+    private
+	// Use this for initialization
+	void Start () {
+        hinge = GetComponent<HingeJoint>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log(transform.rotation.y - 0.1950903);
+        if (transform.rotation.y < minAngle) { hinge.useSpring = true; }
+        else { hinge.useSpring = false; }
+	}
+}

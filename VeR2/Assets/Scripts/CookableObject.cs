ae92a3487c8fde7ee2d8641dfc25981b7b29f691
Brendan Maletski <bamalets@users.noreply.github.com>
2018-11-30 20:09:40 -0500

    Cooking
    Very basic cooking setup. Object put in area, hit button, object gets "cooked"

---
+++ VeR2/Assets/Scripts/CookableObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CookableObject : MonoBehaviour {
+
+    bool IsCooked;
+    Renderer ObjMat;
+
+	// Use this for initialization
+	void Start () {
+        IsCooked = false;
+        ObjMat = transform.GetComponent<Renderer>();
+	}
+
+    public void Cooked() {
+        IsCooked = true;
+        ObjMat.material.color = Color.red;
+    }
+}
---
+++ VeR2/Assets/Scripts/FRecep.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Put the 
+public class FRecep : MonoBehaviour {
+
+    public List<CookableObject> ToCook; //List of stuff that should be cooked
+    public int WhatToDo; //Decides what course of action to take when the button attached to it is hit
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (OVRInput.GetDown(OVRInput.Button.One)) {
+            Cook();
+        }
+	}
+    
+    //When the corresponding button is hit
+    public void Act() {
+        if(WhatToDo == 0) {
+            Cook();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.transform.CompareTag("Cookable")) {
+            CookableObject c = other.transform.GetComponent<CookableObject>(); //The object to be cooked.
+
+            ToCook.Add(c);
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.transform.CompareTag("Cookable")) {
+            CookableObject c = other.transform.GetComponent<CookableObject>(); //The object to be cooked.
+
+            ToCook.Remove(c);
+        }
+    }
+
+    //Cook objects in trigger
+    public void Cook() {
+        if (true) { //Temp spot for making sure the door is locked
+            for(int i = 0; i < ToCook.Count; i++) {
+                ToCook[i].Cooked();
+            }
+        }
+    }
+}

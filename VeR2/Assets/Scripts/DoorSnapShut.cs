f4505f0244373fb339e95dd37e2a754d0c499d0d
Brendan Maletski <bamalets@users.noreply.github.com>
2018-12-07 20:31:57 -0500

    Revert "Code improvements for item spawner and door"
    This reverts commit 64387053c63f29f04a126abce3bceedf7e6a67d8.

--- VeR2/Assets/Scripts/DoorSnapShut.cs
+++ VeR2/Assets/Scripts/DoorSnapShut.cs
@@ -2,20 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-// this code allows a HingeJoint to toggle the spring depending on how close it is to being shut
-
 public class DoorSnapShut : MonoBehaviour {
-    private const double minAngle = 30.0 / 360.0 + 0.1950903; // 30 deg converted to decimal and adjusted to match 0
-    private HingeJoint hinge; // reference to hingeJoint to turn on and off spring
+    private const double minAngle = 30.0 / 360.0 + 0.1950903;
+    private HingeJoint hinge;
     private
 	// Use this for initialization
 	void Start () {
-        hinge = GetComponent<HingeJoint>(); 
+        hinge = GetComponent<HingeJoint>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (transform.rotation.y < minAngle) { hinge.useSpring = true; } // if it is close to shut, turn spring on
-        else { hinge.useSpring = false; } // else, turn spring off so it swings freely
+        //Debug.Log(transform.rotation.y - 0.1950903);
+        if (transform.rotation.y < minAngle) { hinge.useSpring = true; }
+        else { hinge.useSpring = false; }
 	}
 }
